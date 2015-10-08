9d911a2277bb3d7563dae9d56680466f834f81eb
Hananideen <hananee92@gmail.com>
2015-10-08 17:24:26 +0800

    Created app config

---
+++ app/src/main/java/com/pushnotification/app/AppConfig.java
@@ -0,0 +1,11 @@
+package com.pushnotification.app;
+
+/**
+ * Created by Hananideen on 8/10/2015.
+ */
+public class AppConfig {
+    public static final String PARSE_CHANNEL = "Notification";
+    public static final String PARSE_APPLICATION_ID = "KGxzlTUEy77Yyh8RAL1YenCU61sRHnTzc4smfend";
+    public static final String PARSE_CLIENT_KEY = "mZLHR1kVrr6I9Cb7GmC8dFOqiJI0Exvm74VFJBLD";
+    public static final int NOTIFICATION_ID = 100;
+}
