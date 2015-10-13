086ec5b6995d946a0275036c51d04f738195a67e
Hananideen <hananee92@gmail.com>
2015-10-13 10:41:17 +0800

    Created login activity

---
+++ app/src/main/java/com/pushnotification/activity/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.pushnotification.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.pushnotification.R;
+import com.pushnotification.helper.ParseUtils;
+import com.pushnotification.helper.PrefManager;
+
+/**
+ * Created by Hananideen on 13/10/2015.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputEmail;
+    private Button btnLogin;
+    private PrefManager pref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Verifying parse configuration. This is method is for developers only.
+        ParseUtils.verifyParseConfiguration(this);
+
+        pref = new PrefManager(getApplicationContext());
+        if (pref.isLoggedIn()) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
+
+        setContentView(R.layout.activity_login);
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+
+    private void login() {
+        String email = inputEmail.getText().toString();
+
+        if (isValidEmail(email)) {
+
+            pref.createLoginSession(email);
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), "Please enter valid email address!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+    
+}
